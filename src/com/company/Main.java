16e75a49ba9e62049f37b8c6c65cabd2682cf116
Justyna Kocoń <justynkocon95@gmail.com>
2019-01-19 15:41:47 +0100

    Added loop1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,20 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
-        for ( int i=1; i <11; i++){
-            System.out.println(i);// write your code here
+//        System.out.println("Hello World!");
+//
+//        for ( int i=1; i <11; i++){
+//            System.out.println(i);// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj imie: ");
+        String imie = scanner.next();
+        System.out.println("Twoje imie to: " + imie);
+
     }
-}}
+}
